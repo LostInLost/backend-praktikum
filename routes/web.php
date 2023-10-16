d5d990b5af3068b1faf566b64448c73b75cd926b
LostInLost <afanhanan10@gmail.com>
2023-10-16 10:48:08 +0700

    Praktikum 1

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    echo "NIM : 2321770004<br>";
+    echo "Nama : Affansyah Hanan Windharto";
+});
+
+Route::get('/articles/{id}', function($id) {
+    return "Halaman Artikel dengan ID " . $id;
+});
